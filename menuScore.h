aeab6f5b03b5a79e3f0fd3fd5e1abe34cdcf7fa8
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2023-04-04 23:43:32 +0700

    Update menuScore.h

--- menuScore.h
+++ menuScore.h
@@ -6,45 +6,48 @@
 #include <string.h>
 using namespace std;
 
+struct Score {
+	float total_mark;
+	float final_mark;
+	float mid_mark;
+	float other_mark;
+};
 
-struct Student {
-	string No;
+
+struct Student
+{
 	string student_ID;
 	string first_name;
 	string last_name;
 	string gender;
 	string date_of_birth;
 	string social_ID;
-	double fMark;
-	double midMark;
-	double oMark;
-	double toMark;
+
+	Score score;
 	Student* pNext;
 };
+
 struct Course
 {
 	string id;
 	string course_name;
 	string class_name;
-	int number_credits;
-	int number_students;
+	string number_credits;
+	string number_students;
 	string teacher_name;
 	string day_of_week;
 	string sessions;
+
 	Student* student;
 	Course* pNext;
 };
+
+
 struct Semester
 {
 	string semester_name;
 	Course* course;
 	Semester* pNext;
-};
-struct Class
-{
-	string class_name;
-	Student* student;
-	Class* pNext;
 };
 struct SchoolYear
 {
@@ -52,15 +55,9 @@ struct SchoolYear
 	Semester* semester;
 	SchoolYear* pNext;
 };
-void load_student_InClass(Student*& pHead, string class_name);
-void load_class(Class*& pHead);
-void load_student_InCourse(Student*& pHead, string schoolYear_name, string semester_name, string course_name);
-void load_course(Course*& pHead, string schoolYear_name, string semester_name);
-void load_semester(Semester*& pHead, string schoolYear_name);
-void load_schoolYear(SchoolYear*& pHead);
-void load_input(SchoolYear*& pHead_schoolYear);
-void Menu_Score_Board(SchoolYear*& list_year, string year);
-void view_scoreboard_toCourse(SchoolYear* &list_year,string year);
+void view_scoreboard_toCourse(SchoolYear* list_year);
+void import_score_of_course (Course *&z);
+void Menu_Score_Board(SchoolYear*& list_year);
 #endif
 
 
